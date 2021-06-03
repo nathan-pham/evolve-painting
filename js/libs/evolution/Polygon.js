009b6c29c29dcef3c59596729484c3ff19fc225e
Nathan Pham <nathanpham.me@gmail.com>
2021-06-02 17:22:31 -0700

    population & polygon classes

--- js/libs/evolution/Polygon.js
+++ js/libs/evolution/Polygon.js
@@ -1,3 +1,26 @@
-export default class Polygon {
+import { random } from "./math.js"
 
+export default class Polygon {
+    color = `rgba(${random(255)}, ${random(255)}, ${random(255)}, ${random(1)})`
+    vertices = []
+
+    constructor(dimensions, verticeCount) {
+        for(let i = 0; i < verticeCount; i++) {
+            this.vertices.push([random(dimensions.width), random(dimensions.height)])
+        }
+    }
+
+    render(ctx) {
+        ctx.fillStyle = this.color
+        ctx.beginPath()
+    
+        const [genesisX, genesisY] = this.vertices[0]
+        ctx.moveTo(genesisX, genesisY)
+        for(const vertice of this.vertices.slice(1)) {
+            ctx.lineTo(vertice[0], vertice[1])
+        }
+        
+        ctx.closePath()
+        ctx.fill()
+    }
 }
--- js/libs/evolution/Population.js
+++ js/libs/evolution/Population.js
@@ -1,3 +1,32 @@
-export default class Population {
+import Polygon from "./Polygon"
 
+export default class Population {
+    polygons = []
+    fitness = 0
+
+    constructor(dimensions, verticeCount, polygonCount) {
+        for(let i = 0; i < polygonCount; i++) {
+            this.polygons.push(new Polygon(dimensions, verticeCount))
+        }
+    }
+
+    render(ctx) {
+        for(const polygon of this.polygons) {
+            polygon.render(ctx)
+        }
+    }
+
+    calculateFitness(source, result) {
+        let diff = 0
+
+        for (let i = 0; i < sourceData.data.length / 4; i++) {
+            diff += Math.abs(sourceData.data[4 * i + 0] - resultData.data[4 * i + 0]) / 255;
+            diff += Math.abs(sourceData.data[4 * i + 1] - resultData.data[4 * i + 1]) / 255;
+            diff += Math.abs(sourceData.data[4 * i + 2] - resultData.data[4 * i + 2]) / 255;
+        }
+    
+        let fitness = 100 * diff / (sourceData.width * sourceData.height * 3)
+        this.fitness = fitness
+        return fitness
+    }
 }
